db286ac04fcf152bd7336478835c0e333a31b7dd
Spektral01 <chalyhgrisha01@gmail.com>
2023-03-23 20:50:22 +0700

    Add a welcome screen

--- app/src/main/java/com/example/unidorm/MainActivity.kt
+++ app/src/main/java/com/example/unidorm/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.unidorm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.unidorm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.guestBtn.setOnClickListener { onClickGuestLogin() }
+        binding.loginBtn.setOnClickListener { onClickUserLogin() }
     }
+
+    private fun onClickGuestLogin() {
+        val intent = Intent(this, NotificationActivity::class.java)
+        Toast.makeText(this, "WORK",Toast.LENGTH_SHORT).show()
+        startActivity(intent)
+    }
+    private fun onClickUserLogin() {
+
+        Toast.makeText(this, "WORK",Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/unidorm/NotificationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.unidorm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.unidorm.databinding.ActivityNavigationBinding
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+
+}
